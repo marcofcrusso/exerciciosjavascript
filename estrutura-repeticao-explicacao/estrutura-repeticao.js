50bba2077db0d36abba77a6c8fb461439572366c
MARCO FELIPE CORAL RUSSO <35549812819@info.local>
2024-05-13 10:50:45 -0300

    exercicios-estrutura-de-repeticao

---
+++ estrutura-repeticao-explicacao/estrutura-repeticao.js
@@ -0,0 +1,59 @@
+// For - loop contato
+
+
+//contando de 0 a 10
+// for(let num = 0; num <= 10; num++){
+//     console.log(num);   
+// }
+
+// ==================================================================================================
+
+//contagem regressiva
+// for(let cont = 10; cont >= 0; cont--) {
+//     console.log (cont);
+// }
+
+// ===============================================================================================
+
+// //while - loop condicional
+// let salario = 1000;
+// while(salario < 5000){
+//     salario += 100;
+
+//     console.log ("o salario ainda e R$ " + salario)
+// }
+
+// ==================================================================================================
+
+// //Do while - loop condicional
+// let saldo = 1000;
+
+// do{
+//     console.log("esse e o saldo: " + saldo );
+//     saldo += 100;
+
+// }while(saldo < 5000);
+
+// ==================================================================================================
+
+//ARRAY
+//ex: 3 sera pitaya
+// let frutas = ["banana", "laranja", "maca", "pitaya", "melancia"];
+// // console.log(frutas[3]);
+
+// //forEach => percorre uma lista
+// // frutas.forEach(fruta => {
+// //     console.log("A fruta e: " + fruta);
+// // });
+
+// //percorrendo uma lista com for
+// for (let indice = 0; indice < 5; indice++){
+//     console.log(frutas[indice]);
+// }
+
+
+
+
+
+
+
---
+++ exercicios-estrutura-repeticao.js
@@ -0,0 +1,20 @@
+// FOR
+// 1. Imprimir números de 1 a 10 utilizando o loop for
+// 2. Imprimir números pares de 1 a 10.
+// 3. Faça um programa que imprima os números ímpares de 0 a 50;
+// 4. Faça um programa que imprima na tela a tabuada de um número
+// qualquer escolhido pelo usuário até o 10.
+// 5. Imprimir os primeiros 10 números da sequência de Fibonacci.
+
+// WHILE
+// 6. Imprimir números pares de 0 a 20 utilizando o loop while
+// 7. Calcular a soma dos números de 1 a 100 utilizando a estrutura
+// while.
+// 8. Faça um programa que receba a idade do usuário, enquanto a
+// resposta do usuário for menor que 18 anos continue a solicitar a
+// idade e mostre uma mensagem quando a idade do usuário for
+// maior que 18.
+// 9. Faça um programa que receba um número do usuário e mostre
+// no console a contagem regressiva até 0.
+// 10. Faça um programa que receba um número do usuário e continue
+// a pedir o número se o numero for diferente de 0.
