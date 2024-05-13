5d07668ba93c6dbb522f492926f4c40bb154bfbc
MARCO FELIPE CORAL RUSSO <35549812819@info.local>
2024-05-13 08:25:09 -0300

    exercicio7

--- exercicio7.js
+++ exercicio7.js
@@ -6,3 +6,24 @@
 // Valor total da compra de R$ 100,01 a R$ 200,00: desconto de 10%.
 // Valor total da compra acima de R$ 200,00: desconto de 20%.
 // Exiba o valor final da compra com o desconto aplicado.
+
+
+let valortotal = parseFloat(prompt("Informe o valor total da compra"))
+
+let desconto;
+
+if (valortotal <=100){
+    desconto = 0;
+    console.log("o valor de desconto foi: " + desconto);
+}else if(valortotal <= 200){
+    desconto = 0.1;
+    console.log("o valor do desconto foi: " + (desconto*100)+ "%");
+}else {
+    desconto = 0.2;
+    console.log("o valor de desconto foi: " + (desconto*100)+ "%")
+}
+
+let valorfinal = valortotal - (valortotal*desconto);
+
+console.log("o valor final da compra com o desconto: R$" + valorfinal.toFixed(2)); 
+
