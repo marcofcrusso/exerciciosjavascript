6a5032a4788d9afddb0f9ce875ddbe9579760681
MARCO FELIPE CORAL RUSSO <35549812819@info.local>
2024-05-13 08:44:52 -0300

    exercicio8

--- exercicio8.js
+++ exercicio8.js
@@ -8,3 +8,38 @@
 // IMC de 25.0 a 29.9: Sobrepeso
 // IMC de 30.0 a 34.9: Obesidade grau 1
 // IMC de 35.0 a 39.9: Obesidade grau 2
+
+let peso = parseFloat(prompt("informe o seu peso em (kg): "));
+
+let altura = parseFloat(prompt ("informe a sua altura (em metros): "));
+
+let imc = peso / (altura*altura);
+
+console.log("seu IMC e: " + imc);
+
+if(imc < 18.5){
+    console.log("abaixo do peso");
+}
+
+else if (imc >= 18.5 && imc <= 24.9) {
+    console.log("peso normal");
+}
+
+else if (imc >= 25 && imc <= 29.9) {
+    console.log("sobrepeso");
+}
+
+else if (imc >= 30.0 && imc <= 34.9) {
+    console.log("obesidade grau 1");
+}
+
+else if (imc >= 35.0 && imc <= 39.9) {
+    console.log("obesidade grau 2");
+}
+
+else {
+    console.log("obesidade grau 3")
+}
+
+
+
