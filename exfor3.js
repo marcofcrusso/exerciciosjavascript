67668a57b2f6f68b7ff42e2099e212a6740d970e
MARCO FELIPE CORAL RUSSO <35549812819@info.local>
2024-05-14 09:38:57 -0300

    exfor3-javascript

--- exfor3.js
+++ exfor3.js
@@ -1,4 +1,10 @@
 // 3. Faça um programa que imprima os números ímpares de 0 a 50;
 
 
-
+for (var i = 1; i <= 50; i++) {
+    // Verifica se o número é ímpar
+    if (i % 2 != 0) {
+        // Imprime o número ímpar
+        console.log(i);
+    }
+}   
