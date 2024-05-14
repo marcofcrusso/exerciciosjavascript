216c54a1af1f9dd8167800ed773646cbea284f9a
MARCO FELIPE CORAL RUSSO <35549812819@info.local>
2024-05-14 08:49:39 -0300

    exfor2-javascript

--- exfor2.js
+++ exfor2.js
@@ -1,2 +1,9 @@
 // 2. Imprimir números pares de 1 a 10.
 
+for (var i = 1; i <= 10; i++) {
+    // Verifica se o número é par
+    if (i % 2 === 0) {
+        // Se for par, imprime o número
+        console.log(i);
+    }
+}
--- exfor3.js
+++ exfor3.js
@@ -1,1 +1,4 @@
 // 3. Faça um programa que imprima os números ímpares de 0 a 50;
+
+
+
