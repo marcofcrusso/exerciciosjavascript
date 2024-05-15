2730c1bd0d9da2f9874bccde16d5bd2576ca849a
MARCO FELIPE CORAL RUSSO <35549812819@info.local>
2024-05-15 09:38:05 -0300

    exwhile7-javascript

--- exwhile7.js
+++ exwhile7.js
@@ -1,2 +1,15 @@
 // 7. Calcular a soma dos números de 1 a 100 utilizando a estrutura
 // while.
+
+
+
+let num = 1;
+let soma = 0;
+
+while(num <= 100) {
+    soma = soma + num;
+    num++;
+    console.log(soma)
+}
+
+console.log(" A soma dos numeros de 1 a 100 é:" + soma)
