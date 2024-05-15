e262191e00e2d3528ad0ce70d4786152dfb9cf3b
MARCO FELIPE CORAL RUSSO <35549812819@info.local>
2024-05-15 09:43:29 -0300

    exwhile8-javascript

--- exwhile8.js
+++ exwhile8.js
@@ -3,3 +3,13 @@
 // idade e mostre uma mensagem quando a idade do usuário for
 // maior que 18.
 
+
+let idade = parseInt (prompt("Informe sua idade: ")) 
+
+while (idade < 18 ){
+    idade = parseInt(prompt(" Voce e menor de idade. Por favor, digite sua idade novamente: "))
+}   
+
+console.log("Voce e maior de idade. Sua idade e: " + idade);
+
+    
